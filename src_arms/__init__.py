60240fbfea8f2c9cb6a8954c55c38d672a95e097
GonzaloMA-17 <marcos.andres.gonzalo@gmail.com>
2025-02-27 19:38:39 +0100

    iniciado notebook4 - softmax

--- src_arms/__init__.py
+++ src_arms/__init__.py
@@ -1,7 +1,9 @@
 # Importación de módulos o clases
 from .arm import Arm
 from .armNormal import ArmNormal
+from .armBernoulli import ArmBernoulli
+from .armBinomial import ArmBinomial
 from .bandit import Bandit
 
 # Lista de módulos o clases públicas
-__all__ = ['Arm', 'ArmNormal', 'Bandit']
+__all__ = ['Arm', 'ArmNormal', 'Bandit', 'ArmBernoulli', 'ArmBinomial']
